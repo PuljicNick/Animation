b164d66ea6c287020187d7c2b10ff009657b0273
PuljicNick <puljicnick@gmail.com>
2015-04-13 19:21:06 -0400

    Finished

---
+++ src/Halo.java
@@ -0,0 +1,57 @@
+import sprite.Sprite;
+
+/**
+ * Created by puljicn on 4/13/15.
+ */
+public class Halo extends Sprite {
+    public Halo() {
+        super("SpriteH.png");
+
+        createAnimation("standing", 120);
+
+        for (int x = 0; x < 75; x+=50) {
+            addFrame("standing", x, 0, 50, 70);
+        }
+
+        createAnimation("walking", 100);
+
+        for (int x = 163; x < 600; x+=50) {
+            addFrame("walking", x, 0, 48, 70);
+        }
+        addFrame("walking",113,0,48,70);
+        
+        createAnimation("crouch",100);
+
+        for (int x = 100; x < 175; x+=50) {
+            addFrame("crouch", x, 75, 46, 75);
+        }
+        addFrame("crouch",190,75,43,75);
+        addFrame("crouch",235,75,50,75);
+        addFrame("crouch",100,75,46,75);
+
+
+    }
+
+    public void stand() {
+        play("standing");
+        setSpeed(0);
+    }
+
+    public void walkRight() {
+        play("walking");
+        setSpeed(3);
+        setAngle(90);
+        flipHorizontally(false);
+    }
+
+    public void walkLeft() {
+        play("walking");
+        setSpeed(3);
+        setAngle(270);
+        flipHorizontally(true);
+    }
+    public void crouch(){
+        play("crouch", "standing");
+        setSpeed(0);
+    }
+}
--- src/MainApp.java
+++ src/MainApp.java
@@ -6,12 +6,13 @@ import sprite.AnimationPane;
 
 public class MainApp extends Application {
     private AnimationPane animationPane;
+    private Halo halo;
 
     @Override
     public void start(final Stage stage) throws Exception {
         stage.setTitle("Sprite Application");
-        stage.setWidth(600);
-        stage.setHeight(400);
+        stage.setWidth(1024);
+        stage.setHeight(460);
 
         animationPane = new AnimationPane();
         Scene scene = new Scene(animationPane);
@@ -21,17 +22,30 @@ public class MainApp extends Application {
 
         animationPane.setOnKeyPressed(event -> keyPressed(event));
         animationPane.requestFocus();
+        animationPane.setBackgroundImage("Halob.jpg");
 
+        halo = new Halo();
+        animationPane.add(halo);
 
+        halo.setLayoutX(animationPane.getWidth() / 2);
+        halo.setCenterY(animationPane.getHeight() - 120);
 
-
+        halo.stand();
     }
 
 
     private void keyPressed(KeyEvent event) {
         System.out.println(event.getCode().getName());
 
-
+        if (event.getCode().getName().equals("Left")) {
+            halo.walkLeft();
+        } else if (event.getCode().getName().equals("Right")){
+            halo.walkRight();
+        } else if(event.getCode().getName().equals("Down")){
+            halo.crouch();
+        }else {
+            halo.stand();
+        }
 
     }
 
